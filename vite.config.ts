d42b34ea1c411dfca15aae83e986ac377c9d56dc
Mayada Elsayed <mayadaelsayed44@gmail.com>
2026-01-12 12:45:54 +0200

    build for github pages docs

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/event-ticket_new/",
+  build: {
+    outDir: "docs",
+  },
   server: {
     host: "::",
     port: 8080,
