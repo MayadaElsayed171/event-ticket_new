81c14104213013ae1e8686e8bc7d8e1e807e7182
Mayada Elsayed <mayadaelsayed44@gmail.com>
2026-01-12 13:11:20 +0200

    prepare for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,34 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc"; 
+// import path from "path"; import { componentTagger } from "lovable-tagger";
+//  // https://vitejs.dev/config/ 
+//  export default defineConfig(({ mode }) => ({
+//  server: {
+//    host: "::",
+//     port: 8080,
+//    },
+//     plugins: [react(), mode === "development" &&
+//        componentTagger()].filter(Boolean),
+//         resolve: {
+//            alias: { 
+//             "@": path.resolve(__dirname, "./src"),
+//            },
+//            },
+//            }));
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "/event-ticket_new/",
+export default defineConfig({
+  base: "/event-ticket_new/", // اسم الريبو على GitHub
   build: {
-    outDir: "docs",
+    outDir: "docs", // هذا الفولدر اللي هيستخدمه GitHub Pages
   },
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
